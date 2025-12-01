12b28e2e6c336f9d80b53073f4b1acf9a2472767
Younes Beouch <younesbeouch10@gmail.com>
2025-12-01 14:06:30 +0100

    Modification exercice 1.2

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -7,9 +7,9 @@ double rayon = 5.0;
 double aire = M_PI * rayon * rayon;
 double perimetre = 2 * M_PI * rayon;
 
-   printf("Rayon : %.2f", rayon);	  
-   printf("L'air du cercle = %.2f", aire));
-   printf("Le périmètre du cercle  = %.2f", perimetre;
+   printf("Rayon : %.2f ", rayon);	  
+   printf("L'air du cercle = %.2f ", aire);
+   printf("Le périmètre du cercle  = %.2f ", perimetre);
    return 0;
 }
 
